e004d34f1e5ea5e3fa77c9521917620d22fb0242
mayank-mg <mayankgarg148@gmail.com>
2020-10-05 14:40:54 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
 import React from 'react';
 import logo from './logo.svg';
+import Appbar from './component/Menubar/appbar'
+import Drawer from'./component/Drawer/drawer'
+import Tooltip from'./component/Tooltip/tooltip';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   <Appbar />
+   <Drawer />
+   <Tooltip />
     </div>
   );
 }
